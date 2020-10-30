db35bd254da9a2b1ea59f9a4135d9681191932c9
sohumtokekar <sohumtokekar@gmail.com>
2020-10-29 21:00:48 -0500

    Added a constraint log

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+
 }
 
 function setup(){
@@ -35,15 +38,17 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log6.body);
+
+    
 
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    
     box1.display();
     box2.display();
     ground.display();
@@ -60,5 +65,9 @@ function draw(){
     log5.display();
 
     bird.display();
+    log6.display();
     platform.display();
+    chain.display();
+
+
 }
